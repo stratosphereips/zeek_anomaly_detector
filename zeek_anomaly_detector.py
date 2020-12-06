cf2416833aa9ccedfe02a26a6f374cd47c197adf
Sebastian Garcia <eldraco@gmail.com>
2020-12-06 12:22:55 +0100

    Stop using zat, since it was not updated and is not working with new versions of numpy. Now we do everything by hand

--- zeek_anomaly_detector.py
+++ zeek_anomaly_detector.py
@@ -1,35 +1,38 @@
 #!/usr/bin/env python3
 # This file is part of the Stratosphere Linux IPS
 # See the file 'LICENSE' for copying permission.
-# Author: Sebastian Garcia. eldraco@gmail.com , sebastian.garcia@agents.fel.cvut.cz
+# Author: Sebastian Garcia. eldraco@gmail.com,
+# sebastian.garcia@agents.fel.cvut.cz
 
 import pandas as pd
-from zat import bro_log_reader
-from zat.log_to_dataframe import LogToDataFrame
-from sklearn.model_selection import train_test_split
-from pyod.models import lof
-from pyod.models.abod import ABOD
-from pyod.models.cblof import CBLOF
-from pyod.models.lof import LOF
-from pyod.models.loci import LOCI
-from pyod.models.lscp import LSCP
-from pyod.models.mcd import MCD
-from pyod.models.ocsvm import OCSVM
+# from sklearn.model_selection import train_test_split
+# from pyod.models import lof
+# from pyod.models.abod import ABOD
+# from pyod.models.cblof import CBLOF
+# from pyod.models.lof import LOF
+# from pyod.models.loci import LOCI
+# from pyod.models.lscp import LSCP
+# from pyod.models.mcd import MCD
+# from pyod.models.ocsvm import OCSVM
 from pyod.models.pca import PCA
-from pyod.models.sod import SOD
-#from pyod.models.so_gaal import SO_GAAL # Needs keras
-from pyod.models.sos import SOS  # Needs keras
-#from pyod.models.xgbod import XGBOD # Needs keras
-from pyod.models.knn import KNN   # kNN detector
+# from pyod.models.sod import SOD
+# from pyod.models.so_gaal import SO_GAAL # Needs keras
+# from pyod.models.sos import SOS  # Needs keras
+# from pyod.models.xgbod import XGBOD # Needs keras
+# from pyod.models.knn import KNN   # kNN detector
 import argparse
 import warnings
+
 
 # This horrible hack is only to stop sklearn from printing those warnings
 def warn(*args, **kwargs):
     pass
+
+
 warnings.warn = warn
 
-def detect(file, amountanom, realtime,dumptocsv):
+
+def detect(file, amountanom, realtime, dumptocsv):
     """
     Function to apply a very simple anomaly detector
     amountanom: The top number of anomalies we want to print
@@ -37,81 +40,89 @@ def detect(file, amountanom, realtime,dumptocsv):
     """
 
     # Create a Pandas dataframe from the conn.log
-    log_to_df = LogToDataFrame()
-    bro_df = log_to_df.create_dataframe(file,ts_index=False)
-    
-    # In case you need a label, due to some models being able to work in a semisupervized mode, then put it here. For now everything is 'normal', but we are not using this for detection
+    bro_df = pd.read_csv(file, sep="\t", comment='#', names=['ts',  'uid', 'id.orig_h', 'id.orig_p', 'id.resp_h', 'id.resp_p', 'proto', 'service', 'duration',  'orig_bytes', 'resp_bytes', 'conn_state', 'local_orig', 'local_resp', 'missed_bytes',  'history', 'orig_pkts', 'orig_ip_bytes', 'resp_pkts', 'resp_ip_bytes', 'tunnel_parents'])
+
+    # In case you need a label, due to some models being able to work in a
+    # semisupervized mode, then put it here. For now everything is
+    # 'normal', but we are not using this for detection
     bro_df['label'] = 'normal'
 
-    # Change the datetime delta value to seconds. Scikit does not now how to work with timedeltas
-    bro_df['durationsec'] = bro_df.duration.apply(lambda x: x.total_seconds())
-
-    # Replace the rows without data (with '-') with -1. Even though this may add a bias in the algorithms, is better than not using the lines.
-    bro_df['orig_bytes'] = bro_df['orig_bytes'].fillna(0)
-    bro_df['resp_bytes'] = bro_df['resp_bytes'].fillna(0)
-    bro_df['resp_pkts'] = bro_df['resp_pkts'].fillna(0)
-    bro_df['orig_ip_bytes'] = bro_df['orig_ip_bytes'].fillna(0)
-    bro_df['resp_ip_bytes'] = bro_df['resp_ip_bytes'].fillna(0)
-    bro_df['durationsec'] = bro_df['durationsec'].fillna(0)
+    # Replace the rows without data (with '-') with 0.
+    # Even though this may add a bias in the algorithms,
+    # is better than not using the lines.
+    # Also fill the no values with 0
+    # Finally put a type to each column
+    bro_df['orig_bytes'].replace('-', '0', inplace=True)
+    bro_df['orig_bytes'] = bro_df['orig_bytes'].fillna(0).astype('int32')
+    bro_df['resp_bytes'].replace('-', '0', inplace=True)
+    bro_df['resp_bytes'] = bro_df['resp_bytes'].fillna(0).astype('int32')
+    bro_df['resp_pkts'].replace('-', '0', inplace=True)
+    bro_df['resp_pkts'] = bro_df['resp_pkts'].fillna(0).astype('int32')
+    bro_df['orig_ip_bytes'].replace('-', '0', inplace=True)
+    bro_df['orig_ip_bytes'] = bro_df['orig_ip_bytes'].fillna(0).astype('int32')
+    bro_df['resp_ip_bytes'].replace('-', '0', inplace=True)
+    bro_df['resp_ip_bytes'] = bro_df['resp_ip_bytes'].fillna(0).astype('int32')
+    bro_df['duration'].replace('-', '0', inplace=True)
+    bro_df['duration'] = bro_df['duration'].fillna(0).astype('float64')
 
     # Save dataframe to disk as CSV
     if dumptocsv != "None":
         bro_df.to_csv(dumptocsv)
 
     # Add the columns from the log file that we know are numbers. This is only for conn.log files.
-    X_train = bro_df[['durationsec', 'orig_bytes', 'id.resp_p', 'resp_bytes', 'orig_ip_bytes', 'resp_pkts', 'resp_ip_bytes']]
+    X_train = bro_df[['duration', 'orig_bytes', 'id.resp_p', 'resp_bytes', 'orig_ip_bytes', 'resp_pkts', 'resp_ip_bytes']]
 
     # Our y is the label. But we are not using it now.
     y = bro_df.label
 
-    # The X_test is where we are going to search for anomalies. In our case, its the same set of data than X_train. 
+    # The X_test is where we are going to search for anomalies. In our case, its the same set of data than X_train.
     X_test = X_train
 
     #################
     # Select a model from below
 
-    # ABOD class for Angle-base Outlier Detection. For an observation, the variance of its weighted cosine scores to all neighbors could be viewed as the outlying score.
-    #clf = ABOD()
+    # ABOD class for Angle-base Outlier Detection. For an observation, the
+    # variance of its weighted cosine scores to all neighbors could be
+    # viewed as the outlying score.
+    # clf = ABOD()
 
     # LOF
-    #clf = LOF()
+    # clf = LOF()
 
     # CBLOF
-    #clf = CBLOF()
+    # clf = CBLOF()
 
     # LOCI
-    #clf = LOCI()
-    
+    # clf = LOCI()
+
     # LSCP
-    #clf = LSCP()
+    # clf = LSCP()
 
     # MCD
-    #clf = MCD()
+    # clf = MCD()
 
     # OCSVM
-    #clf = OCSVM()
+    # clf = OCSVM()
 
     # PCA. Good and fast!
     clf = PCA()
 
-    # SOD 
-    #clf = SOD()
+    # SOD
+    # clf = SOD()
 
     # SO_GAAL
-    #clf = SO_GALL()
+    # clf = SO_GALL()
 
     # SOS
-    #clf = SOS()
+    # clf = SOS()
 
     # XGBOD
-    #clf = XGBOD()
-
-
+    # clf = XGBOD()
 
     # KNN
     # Good results but slow
-    #clf = KNN()
-    #clf = KNN(n_neighbors=10)
+    # clf = KNN()
+    # clf = KNN(n_neighbors=10)
     #################
 
     # Fit the model to the train data
@@ -139,16 +150,14 @@ def detect(file, amountanom, realtime,dumptocsv):
     # Keep the top X amount of anomalies
     top10 = X_test_predicted.sort_values(by='score', ascending=False).iloc[:amountanom]
 
-    ## Print the results
+    # Print the results
     # Find the predicted anomalies in the original bro dataframe, where the rest of the data is
-    #df_to_print = bro_df.iloc[top10.index]
     df_to_print = bro_df.iloc[top10.index]
     print('\nFlows of the top anomalies')
 
     # Only print some columns, not all, so its easier to read.
-    df_to_print = df_to_print.drop(['conn_state','history','local_orig' ,'local_resp' ,'missed_bytes' ,'ts', 'tunnel_parents' ,'uid' ,'label' ], axis=1)
+    df_to_print = df_to_print.drop(['conn_state', 'history', 'local_orig', 'local_resp', 'missed_bytes', 'ts', 'tunnel_parents', 'uid', 'label'], axis=1)
     print(df_to_print)
-
 
 
 if __name__ == '__main__':
@@ -166,5 +175,3 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     detect(args.file, args.amountanom, args.realtime, args.dumptocsv)
-
-
